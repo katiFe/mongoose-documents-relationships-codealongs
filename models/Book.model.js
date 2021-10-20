017501e665547f9d5d16a4cdd2095bd24a707553
Kati Fe <k-felgitsch@hotmail.com>
2021-10-20 10:58:23 +0100

    setting up relation to Author.model

--- models/Book.model.js
+++ models/Book.model.js
@@ -1,10 +1,11 @@
 const { Schema, model } = require('mongoose');
 
+
 const bookSchema = new Schema(
   {
     title: String,
     description: String,
-    author: String,
+    author: {type: Schema.Types.ObjectId, ref: 'Author'},  //referencing authors
     rating: Number
   },
   {
